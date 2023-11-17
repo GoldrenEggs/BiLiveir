61e42b7c9d2391d77908ef0c15a135cd2ea09dda
gwq <guowanqi@tianchuangsec.com>
2023-11-17 18:00:54 +0800

    不加typehint还默认为None的包真该死啊

--- src/connect.py
+++ src/connect.py
@@ -1,96 +1,26 @@
-import json
-from enum import Enum
-from struct import pack, unpack
-from typing import Protocol
-from dataclasses import dataclass
+from aiowebsocket.converses import AioWebSocket, Converse
 
-
-class Packet(Protocol):
-    def pack(self) -> bytes:
-        ...
-
-
-class ProtocolVersion(Enum):
-    NORMAL = 0
-    AUTH_AND_HEARTBEAT = 1
-    NORMAL_ZLIB = 2
-    NORMAL_BROTLI = 3
-
-
-class OperationCode(Enum):
-    HEARTBEAT = 2
-    HEARTBEAT_REPLY = 3
-    NORMAL = 5
-    AUTH = 7
-    AUTH_REPLY = 8
-
-
-class MQPacket:
-
-    def __init__(self, protocol_version: ProtocolVersion, operation_code: OperationCode, sequence: int, data: bytes):
-        self.packet_length = 16 + len(data)
-        self.header_length = 16
-        self.protocol_version = protocol_version
-        self.operation_code = operation_code
-        self.sequence = sequence
-        self.data = data
-
-    def pack(self):
-        return pack('!IHHII', self.packet_length, self.header_length, self.protocol_version.value,
-                    self.operation_code.value, self.sequence) + self.data
-
-
-# noinspection SpellCheckingInspection
-@dataclass
-class AuthData:
-    room_id: int
-    uid: int = 0
-    protover: int = 3
-    platform: str = 'web'
-    type: int = 2
-    key: str = ''
-
-    def __bytes__(self):
-        return bytes(json.dumps({
-            'uid': self.uid,
-            'room_id': self.room_id,
-            'protover': self.protover,
-            'platform': self.platform,
-            'type': self.type,
-            'key': self.key
-        }), encoding='utf-8')
-
-
-class AuthPacket(MQPacket):
-    def __init__(self, sequence: int, data: AuthData):
-        super().__init__(ProtocolVersion.AUTH_AND_HEARTBEAT, OperationCode.AUTH, sequence, bytes(data))
-
-    def pack(self):
-        return super().pack()
-
-
-@dataclass
-class HeartbeatData:
-    message: str
-
-    def __bytes__(self):
-        return self.message.encode('utf-8')
-
-
-class HeartbeatPacket(MQPacket):
-    def __init__(self, sequence: int, data: HeartbeatData):
-        super().__init__(ProtocolVersion.AUTH_AND_HEARTBEAT, OperationCode.HEARTBEAT, sequence, bytes(data))
-
-    def pack(self) -> bytes:
-        return super().pack()
+from src.packet import AuthPacket, HeartbeatPacket, AuthData, HeartbeatData
 
 
 class LiveConnect:
-    ...
+    URI = 'ws://broadcastlv.chat.bilibili.com:2244/sub'
+
+    def __init__(self, room_id: int):
+        self.room_id = room_id
+        self.sequence = 0
+        self.aws = AioWebSocket(self.URI)
+
+    async def send_auth_packet(self):
+        self.sequence += 1
+        packet = AuthPacket(self.sequence, AuthData(self.room_id))
+
+    async def connect(self):
+        await self.aws.create_connection()
+        await self.aws.converse
 
 
 if __name__ == '__main__':
-    auth_packet = AuthPacket(1, AuthData(184854)).pack()
-    print(auth_packet)
-    heartbeat_packet = HeartbeatPacket(1, HeartbeatData('')).pack()
-    print(heartbeat_packet)
+    import asyncio
+
+    asyncio.run(LiveConnect(8641).connect())
